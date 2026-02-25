4ca3ee5a36592c228747a4af0b6e1b01f51edff3
VITORIA SOUZA DA SILVA <vitoria.souza.silva03@escola.pr.gov.br>
2026-02-25 11:40:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+print("olá mundo")
+#02
+num1=(input("digite o numero:"))
+print(f"o número informado foi:{num1}")
+#03
+num1 = int(input("digite o primeiro número:"))
+num2 = int(input("digite o segundo número:"))
+print(f"{num1} + {num2} = {num1 + num2}")
+"""
