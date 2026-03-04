4cbf311c5386ff267780ef914d4f20acd806b31a
vitoria souza da silva <54339047-vitoriasouzasil@users.noreply.replit.com>
2026-03-04 11:07:56 +0000

    trabalho do python

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
-"""
-print("olá mundo")
-#02
-num1=(input("digite o numero:"))
-print(f"o número informado foi:{num1}")
-#03
-num1 = int(input("digite o primeiro número:"))
-num2 = int(input("digite o segundo número:"))
-print(f"{num1} + {num2} = {num1 + num2}")
-"""
+#Ex 1
+#print("Olá mundo!")
+#Ex 2
+#num = input("Digite um número: ")
+#print("O número informado foi: ", num)
+#Ex 3
+#num1 = input("Digite o primeiro número: ")
+#num2 = input("Digite o segundo número: ")
+#soma = int(num1) + int(num2)
+#print("A soma dos números é: ", soma)
+#Ex 4
+#nota1 = input("Digite a primeira nota: ")
+#nota2 = input("Digite a segunda nota: ")
+#nota3 = input("Digite a terceira nota: ")
+#nota4 = input("Digite a quarta nota: ")
+#media = (int(nota1) + int(nota2) + int(nota3) + int(nota4)) / 4
+#print("A média das notas é: ", media)
+#Ex 5
+#metros = input("Digite o valor em metros: ")
+#centimetros = int(metros) * 100
+#print("O valor em centímetros é: ", centimetros)
+#Ex 6
+#raio  = input("Digite o raio do círculo: ")
+#area = 3.14 * (int(raio) ** 2)
+#print("A área do círculo é: ", area)
+#Ex 7
+#area_do_quadrado = input("Digite a área do quadrado: ")
+#area = int(area_do_quadrado) ** 2
+#dobro = area * 2
+#print("O dobro da área do quadrado é: ", dobro)
+#Ex 8
+#print("Quanto você ganha por hora?")
+#salario_hora = input()
+#print("Quantas horas você trabalha por mês?")
+#horas_mes = input()
+#salario_mes = int(salario_hora) * int(horas_mes)
+#print("Seu salário mensal é: ", salario_mes)
+#Ex 9
+#valor_inicial = input("Digite o valor inicial: ")
+#desconto = input("Digite o valor do desconto: ")
+#valor_final = int(valor_inicial) - int(desconto)
+#print("O valor final é: ", valor_final)
+#Ex 10
+raio_do_cone = input("Digite o raio do cone: ")
+altura_do_cone = input("Digite a altura do cone: ")
+volume_do_cone = (3.14 * (int(raio_do_cone) ** 2) * int(altura_do_cone)) / 3
+print("O volume do cone é: ", volume_do_cone)
